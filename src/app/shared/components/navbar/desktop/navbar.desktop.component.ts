3aabd81b4ec441b0f84cae7182a623c12415f4ac
camilo3312 <ccvelasquez531@misena.edu.co>
2023-03-02 17:55:49 -0500

    ADD: dark mode to desktop design

--- src/app/shared/components/navbar/desktop/navbar.desktop.component.ts
+++ src/app/shared/components/navbar/desktop/navbar.desktop.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from '@app/shared/services';
 
 @Component({
   selector: 'app-navbar-desktop',
   templateUrl: './navbar.desktop.component.html',
   styleUrls: ['./navbar.desktop.component.scss']
 })
-export class NavbarDesktopComponent {}
+export class NavbarDesktopComponent {
+  constructor(
+    public theme: ThemeService
+  ) {}
+
+}
